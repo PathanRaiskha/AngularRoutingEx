68d07635a8d8d9f0dcfa023209ea8a9ab986992a
Pathan Raiskha Rubab <pathanrais8407@gmail.com>
2025-07-14 21:34:12 +0530

    Add files via upload

---
+++ input-and-output.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-and-output',
+  imports: [],
+  templateUrl: './input-and-output.component.html',
+  styleUrl: './input-and-output.component.css'
+})
+export class InputAndOutputComponent {
+
+}
